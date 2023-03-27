340813cdec7300c87a82677ddbcf53d1fd85ae1a
Manish <manish.paul@stud.unifi.it>
2023-03-27 17:43:10 +0200

    Added save function

--- version.h
+++ version.h
@@ -4,5 +4,7 @@
 
 #ifndef GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
 #define GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
+#include <string>
+static const std::string version = "v1.0";
 
 #endif //GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
